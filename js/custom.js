c0a85a6b820f7aaa7db9c4ef132467125c8c8e25
James Sudhir <james.sudhir@ithands.biz>
2018-11-26 11:12:34 +0530

    added mneu

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
 
 //Scroll to Section
     $(document).ready(function(){
